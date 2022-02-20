d97142f7245dbf0bcc529e86a9715ad24ffde616
MPRGDAW1920 <rodes20257@iesmarenostrum.com>
2022-02-20 09:36:31 +0100

    Solucion creada

---
+++ 12002SOLMPRG2122/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12002SOLMPRG2122
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
